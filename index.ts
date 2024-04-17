bb2efe7c40de9c90b66b4356cf451d7bcbc9ee6b
Walid Mostefaoui <pro.walid.mostefaoui@gmail.com>
2024-04-18 00:49:32 +0200

    Added S3 Bucket

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
 import express, { Express, Request, Response } from 'express'
 import 'dotenv/config'
+import { createBucket, getS3Client } from './src/upload'
+import { ListBucketsCommand } from '@aws-sdk/client-s3'
 
 const app: Express = express()
 
-app.get('/', (_: Request, res: Response) => {
+const bucketName = `test-bucket-${Date.now()}`
+createBucket(bucketName)
+createBucket(bucketName + 2)
+
+app.get('/', async (_: Request, res: Response) => {
+	const command = new ListBucketsCommand({})
+
+	const client = getS3Client()
+	const { Buckets } = await client.send(command)
+	if (!Buckets) return 'no buckets'
+	console.log('Buckets: ')
+	console.log(Buckets.map(bucket => bucket.Name).join('\n'))
 	res.send('Hello World!')
 })
 
---
+++ src/upload.ts
@@ -0,0 +1,98 @@
+// This is used for getting user input.
+import { createInterface } from 'readline/promises'
+
+import {
+	S3Client,
+	PutObjectCommand,
+	CreateBucketCommand,
+	DeleteObjectCommand,
+	DeleteBucketCommand,
+	paginateListObjectsV2,
+	GetObjectCommand,
+} from '@aws-sdk/client-s3'
+
+// A region and credentials can be declared explicitly. For example
+// `new S3Client({ region: 'us-east-1', credentials: {...} })` would
+//initialize the client with those settings. However, the SDK will
+// use your local configuration and credentials if those properties
+// are not defined here.
+const s3Client = new S3Client({})
+
+export const getS3Client = () => {
+	return s3Client
+}
+
+export const createBucket = async (bucketName: string) => {
+	// Create an Amazon S3 bucket. The epoch timestamp is appended
+	// to the name to make it unique.
+	const bucket = await s3Client.send(
+		new CreateBucketCommand({
+			Bucket: bucketName,
+		}),
+	)
+
+	return bucket
+}
+
+export const sendDocument = async (bucketName: string) => {
+	// Put an object into an Amazon S3 bucket.
+	await s3Client.send(
+		new PutObjectCommand({
+			Bucket: bucketName,
+			Key: 'my-first-object.txt',
+			Body: 'Hello JavaScript SDK!',
+		}),
+	)
+}
+
+export const getDocument = async (bucketName: string) => {
+	// Read the object.
+	const { Body } = await s3Client.send(
+		new GetObjectCommand({
+			Bucket: bucketName,
+			Key: 'my-first-object.txt',
+		}),
+	)
+
+	if (!Body) throw new Error('S3 Client sent an error')
+
+	console.log(await Body.transformToString())
+
+	return Body
+}
+
+export const deleteBucket = async (bucketName: string) => {
+	// Confirm resource deletion.
+	const prompt = createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	})
+
+	const result = await prompt.question('Empty and delete bucket? (y/n) ')
+	prompt.close()
+
+	if (result === 'y') {
+		// Create an async iterator over lists of objects in a bucket.
+		const paginator = paginateListObjectsV2(
+			{ client: s3Client },
+			{ Bucket: bucketName },
+		)
+		for await (const page of paginator) {
+			const objects = page.Contents
+			if (objects) {
+				// For every object in each page, delete it.
+				for (const object of objects) {
+					await s3Client.send(
+						new DeleteObjectCommand({
+							Bucket: bucketName,
+							Key: object.Key,
+						}),
+					)
+				}
+			}
+		}
+
+		// Once all the objects are gone, the bucket can be deleted.
+		await s3Client.send(new DeleteBucketCommand({ Bucket: bucketName }))
+	}
+}
